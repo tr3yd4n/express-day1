5e3f800ffc41f1b15ac28fd2a6c9d56954f5248e
tr3yd4n <80318628+tr3yd4n@users.noreply.github.com>
2021-06-22 23:37:02 +0100

    Add files via upload

--- src/authors/index.js
+++ src/authors/index.js
@@ -28,8 +28,11 @@ router.get('/:id', async (req, res, next) => {
         const fileAsBuffer = fs.readFileSync(authorsFilePath)
         const fileAsString = fileAsBuffer.toString()
         const fileAsJson = JSON.parse(fileAsString)
-        const author = fileAsJson.filter(req.param.id)
-        res.sendStatus(200).send(author)
+        const author = fileAsJson.find(author => author.id === req.params.id)
+        if (!author) {
+            res.sendStatus(404).send({ message: "Author with ${req.params.id} not found!" })
+        }
+        res.send(author)
     } catch (error) {
         res.sendStatus(500).send({ message: error.message })
     }
@@ -64,18 +67,38 @@ router.post('/', async (req, res, next) => {
 //ammend author
 router.put('/:id', async (req, res, next) => {
     try {
-
+        const fileAsBuffer = fs.readFileSync(authorsFilePath)
+        const fileAsString = fileAsBuffer.toString()
+        let fileAsJson = JSON.parse(fileAsString)
+        const authorIndex = fileAsJson.findIndex(author => author.id === req.params.id)
+        if (!authorIndex == -1) {
+            res.sendStatus(404).send({ message: "Author with ${req.params.id} not found!" })
+        }
+        const previousAuthorData = fileAsJson[authorIndex]
+        const changedAuthor = { ...previousAuthorData, ...req.body, updatedAt: new Date(), id: req.params.id }
+        fileAsJson[authorIndex] = changedAuthor
+        fs.writeFileSync(authorsFilePath, JSON.stringify(fileAsJson))
+        res.send(changedAuthor)
     } catch (error) {
-        res.send(500).send({ message: error.message })
+        res.sendStatus(500).send({ message: error.message })
     }
 })
 
 //delete author
 router.delete('/:id', async (req, res, next) => {
     try {
-
+        const fileAsBuffer = fs.readFileSync(authorsFilePath)
+        const fileAsString = fileAsBuffer.toString()
+        let fileAsJson = JSON.parse(fileAsString)
+        const author = fileAsJson.find(author => author.id === req.params.id)
+        if (!author) {
+            res.sendStatus(404).send({ message: "Author with ${req.params.id} not found!" })
+        }
+        fileAsJson = fileAsJson.filter((author) => author.id !== req.params.id)
+        fs.writeFileSync(authorsFilePath, JSON.stringify(fileAsJson))
+        res.sendStatus(204).send({ message: "Deleted" })
     } catch (error) {
-        res.send(500).send({ message: error.message })
+        res.sendStatus(500).send({ message: error.message })
     }
 })
 
