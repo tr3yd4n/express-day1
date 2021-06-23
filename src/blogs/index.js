199c12e460829132a7103de8b2b6a7231267d71b
tr3yd4n <80318628+tr3yd4n@users.noreply.github.com>
2021-06-23 14:22:29 +0100

    Add files via upload

--- src/blogs/index.js
+++ src/blogs/index.js
@@ -6,13 +6,12 @@ import uniqid from "uniqid"
 import createError from "http-errors"
 
 const router = express.Router()
-
 const blogsJsonPath = join(dirname(fileURLToPath(import.meta.url)), "blogs.json")
-
 const getBlogsArray = () => {
     const content = fs.readFileSync(blogsJsonPath)
     return JSON.parse(content)
 }
+const writeBlogs = content => fs.writeFileSync(blogsJsonPath, JSON.stringify(content))
 
 router.get("/", async (req, res, next) => {
     try {
@@ -23,4 +22,50 @@ router.get("/", async (req, res, next) => {
     }
 })
 
+router.get("/:id", async (req, res, next) => {
+    try {
+        const blogs = getBlogsArray()
+        const blog = blogs.find(blog => blog.id === req.params.id)
+        res.send(blog)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.post("/", async (req, res, next) => {
+    try {
+        const newBlog = { ...req.body, id: uniqid(), createdAt: new Date() }
+        const blogs = getBlogsArray()
+        blogs.push(newBlog)
+        writeBlogs(blogs)
+        res.status(201).send(newBlog)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.put("/:id", async (req, res, next) => {
+    try {
+        const blogs = getBlogsArray()
+        const remainingBlogs = blogs.filter(blog => blog.id !== req.params.id)
+        const updatedBlog = { ...req.body, id: req.params.id }
+        remainingBlogs.push(updatedBlog)
+        writeBlogs(remainingBlogs)
+        res.send(updatedBlog)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const blogs = getBlogsArray()
+        const remainingBlogs = blogs.filter(blog => blog.id !== req.params.id)
+        writeBlogs(remainingBlogs)
+        res.status(200).send("deleted!")
+
+    } catch (error) {
+        next(error)
+    }
+})
 export default router
